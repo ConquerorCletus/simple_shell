eca28a5d35d7f09d457497b508378926279b76a6
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-12 20:32:34 +0000

    getline and display prompt added to main.c

--- main.c
+++ main.c
@@ -7,5 +7,30 @@
  */
 int main(int ac, char **argv)
 {
+	char *displayptr = "~simpleShell$ ", *lineptr;
+	size_t bytes = 0;
+
+	(void)ac;
+	(void)argv;
+
+
+
+	printf("%s", displayptr);
+	getline(&lineptr, &bytes, stdin);
+	printf("%s\n", lineptr);
+
+	free(lineptr);
+
+
+
+
+
+
+
+
+
+
+
+
 	return (0);
 }
