c61d2621a4cf368ff33613b59254b38ce230347b
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-12 19:59:10 +0000

    main.c and main.h

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - A simple shell program
+ * @ac: arument count
+ * @argv: pointer to argument vector
+ * Return: 0
+ */
+int main(int ac, char **argv)
+{
+	return (0);
+}
