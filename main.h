3c9452c9f8d51d2174d12231e042708d47918cd7
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-14 07:29:11 -0700

    Authors/main.h updates

--- main.h
+++ main.h
@@ -5,14 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-
-
-
-
-
-
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
 
 
 
